40bc6197c644a8ad9c0615832ef564f2e0a14c10
Benjamin Aho <97265527+Baho717@users.noreply.github.com>
2023-12-21 13:14:00 -0500

    Create CaesarCipherTests.cs
    initial unit tests added

---
+++ CaesarCipherTests.cs
@@ -0,0 +1,34 @@
+using Xunit;
+using CaesarCipher.cs;
+
+public class CaesarCipherTests
+{
+    private static readonly char[] Alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    [Fact]
+    public void EncryptMessage_WithLowerCaseString_ReturnsEncryptedString()
+    {
+        // Arrange
+        var input = "abc";
+        var expected = "def"; 
+        // Act
+        var result = Program.EncryptMessage(input, Alphabet);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void EncryptMessage_WithUpperCaseString_ReturnsEncryptedString()
+    {
+        // Arrange
+        var input = "ABC";
+        var expected = "DEF";
+
+        // Act
+        var result = Program.EncryptMessage(input, Alphabet);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
